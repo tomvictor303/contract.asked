94b27b5e35dcb82c987160e68aaf284eacf64e57
tomvictor303 <tomvictor303@gmail.com>
2024-06-04 07:28:12 -0400

    rate logic

--- contracts/FreelancePlatform.sol
+++ contracts/FreelancePlatform.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.0;
 
 contract FreelancePlatform {
     address public owner;
+    uint256 private rate;
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event RateUpdated(uint256 oldRate, uint256 newRate);
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Caller is not the owner");
@@ -18,8 +20,17 @@ contract FreelancePlatform {
     
     function changeOwner(address newOwner) public onlyOwner {
         require(newOwner != address(0), "New owner is the zero address");
-        emit OwnershipTransferred(owner, newOwner);
         owner = newOwner;
+        emit OwnershipTransferred(owner, newOwner);
+    }
+
+    function setRate(uint256 newRate) public onlyOwner {
+        rate = newRate;
+        emit RateUpdated(rate, newRate);
+    }
+
+    function getRate() public view returns (uint256) {
+        return rate;
     }
 
     function isOwner(address _address) public view returns (bool) {
