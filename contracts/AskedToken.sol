5c0afb0493fa4b919e4f3bf1a768406ab775de55
tomvictor303 <tomvictor303@gmail.com>
2024-06-06 11:13:52 -0400

    initial mint unit in AskedToken

--- contracts/AskedToken.sol
+++ contracts/AskedToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract AskedToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Asked", "ASK") {
-        _mint(msg.sender, initialSupply);
+    constructor(uint256 initialSupplyByIntegerUnit) ERC20("Asked", "ASK") {
+        _mint(msg.sender, initialSupplyByIntegerUnit * 10 ** 18);
     }
 }
