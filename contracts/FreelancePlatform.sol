bb8a3cd6b6c0b7907dd5837b9ba75e163d4fb87b
tomvictor303 <tomvictor303@gmail.com>
2024-06-13 22:59:11 -0400

    update code

--- contracts/FreelancePlatform.sol
+++ contracts/FreelancePlatform.sol
@@ -21,7 +21,9 @@ contract FreelancePlatform is BasePlatform {
 
     // Mapping from job ID to Job struct
     mapping(uint256 => Job) public jobs;
-    uint256 public nextJobId;
+
+    // Mapping from client address to list of job IDs
+    mapping(address => uint256[]) public jobsFromClient;
 
     // ASK token contract
     IERC20 public askToken;
@@ -37,9 +39,9 @@ contract FreelancePlatform is BasePlatform {
     }
 
     // Create a new job and transfer ASK tokens from client to contract
-    function createJob(address freelancer, uint256 budget) public {
+    function createJob(uint256 jobId, address freelancer, uint256 budget) public {
         uint256 currentTime = block.timestamp;
-        jobs[nextJobId] = Job({
+        jobs[jobId] = Job({
             client: msg.sender,
             freelancer: freelancer,
             budget: budget,
@@ -48,36 +50,37 @@ contract FreelancePlatform is BasePlatform {
             isRefunded: false,
             status: JobStatus.Offer
         });
-        emit JobCreated(nextJobId, msg.sender, budget, currentTime);
+        emit JobCreated(jobId, msg.sender, budget, currentTime);
+
+        // Push job ID to client's list of jobs
+        jobsFromClient[msg.sender].push(jobId);
         
         // Transfer ASK tokens from client to contract
         require(askToken.transferFrom(msg.sender, address(this), budget), "Token transfer failed");
-
-        nextJobId++;
     }
 
     // Update job status and transfer ASK tokens to freelancer if job is completed
-    function updateJobStatus(uint256 jobId, JobStatus status) public {
+    function completeJob(uint256 jobId) public {
         Job storage job = jobs[jobId];
         require(msg.sender == job.client || msg.sender == job.freelancer, "Caller is not authorized");
-        job.status = status;
-        emit JobUpdated(jobId, status);
+        job.status = JobStatus.Completed;
 
         // Transfer ASK tokens to freelancer if job is completed
-        if (status == JobStatus.Completed) {
-            require(askToken.transfer(job.freelancer, job.budget), "Token transfer to freelancer failed");
-        }
+        require(askToken.transfer(job.freelancer, job.budget), "Token transfer to freelancer failed");
+        emit JobUpdated(jobId, JobStatus.Completed);
     }
 
     // Mark job as refunded and return ASK tokens to client
     function refundJob(uint256 jobId) public onlyOwner {
         Job storage job = jobs[jobId];
+        require(msg.sender == job.client, "Caller is not authorized. Only client can refund.");
         require(!job.isRefunded, "Job is already refunded");
         job.isRefunded = true;
-        emit JobRefunded(jobId, true);
+        job.status = JobStatus.Canceled;
 
         // Transfer ASK tokens back to client
         require(askToken.transfer(job.client, job.budget), "Token refund failed");
+        emit JobRefunded(jobId, true);
     }
 
     // Get job details
